d111702687d3fe3dfcc84f67204453c7b78f2598
1aviervargas <jvargasbalbuena@gmail.com>
2023-11-04 20:53:53 +0100

    aa

---
+++ install.m
@@ -0,0 +1,11 @@
+%Add Matlab Path:
+addpath('dipimage_2.9_lin64/dip/common/mlv7_6/diplib')
+addpath('dipimage_2.9_lin64/dip/common/mlv7_6/dipimage_mex')
+addpath('dipimage_2.9_lin64/dip/common/dipimage/aliases')
+addpath('dipimage_2.9_lin64/dip/common/dipimage')
+
+%With subfolders
+addpath('/home/jvargas/Software/InSilicoTEM-v2.0.0');
+
+dip_initialise
+dipsetpref('imagefilepath','/herebeimages')
